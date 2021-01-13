9151358e7d2ec1df06a8200347287e90c04d9fc0
Nadeem Bhati <bhatindm@gmail.com>
2021-01-13 12:53:00 +0530

    new changes

--- src/main/java/com/bikemongo/api/BikeMongoApplication.java
+++ src/main/java/com/bikemongo/api/BikeMongoApplication.java
@@ -2,6 +2,9 @@ package com.bikemongo.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BikeMongoApplication {
@@ -9,5 +12,13 @@ public class BikeMongoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BikeMongoApplication.class, args);
 	}
-
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
+	}
 }
